a253d30770a784c8495c30835f6579b6e7031ae8
Maksim Kosach <mx.kosach@gmail.com>
2022-03-02 22:15:26 +0300

    Fix bugs

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.xml.ws.FaultAction;
 import java.util.*;
 
 //Ввести с консоли n -размерность матрицы а[n][n]
@@ -15,70 +14,86 @@ public class Main {
      * Задать значения элементов матрицы в интервале значений от -n до n с помощью генератора случайных чисел.
      * Найти максимальный элемент(ы) в матрице и удалить из матрицы все строки и столбцы, его содержащие.
      */
+    public static void showAuthorAndDate(Date dateOfReceipt) {
+        Date dateNow = new Date();
+        System.out.println("\n***********************");
+        System.out.println("©Maksim Kosach, 2022");
+        System.out.println("Date of receipt of the task: " + dateOfReceipt);
+        System.out.println("Date of completion of the task: " + dateNow);
+    }
 
     public static void main(String[] args) {
+        Date date1 = new Date(1645086000000L);
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the dimension of the matrix and press <Enter>:");
-        int n = scan.nextInt();
+        int dim = scan.nextInt();
         scan.close();
 
-        int matrix[][] = new int[n][n];
+        int matrix[][] = new int[dim][dim];
         Random random = new Random();
-       // List<List<Integer>> list= new ArrayList<>();
+        //List<List<Integer>> list= new ArrayList<>();
 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = random.nextInt(2 * n + 1) - n;
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                matrix[i][j] = random.nextInt(2 * dim + 1) - dim;
             }
         }
         System.out.println("Original matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 System.out.printf("%4d", matrix[i][j]);
             }
             System.out.println();
         }
 
-        int max = -n;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        int max = -dim;
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 if (matrix[i][j] > max)
                     max = matrix[i][j];
             }
         }
         System.out.println("Maximum element of the matrix: " + max);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        //////////////////////////////////////////////////////////////////////////////
+        int dimNew = dim;
+        ;
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 {
                     if (matrix[i][j] == max) {
-                        for (int k = 0; k < n; k++) {
-                            // matrix[i][k] = null;
+                        for (int k = 0; k < dim; k++) {
+                            matrix[i][k] = -dim - 1;
                         }
-                        for (int k = 1; k < n; k++) {
-                            //matrix[k][j] = null;
+                        for (int k = 0; k < dim; k++) {
+                            matrix[k][j] = -dim - 1;
                         }
+                        //matrix[i][j]=-dim-1;
+                        //dimNew--;
+                        //break;
                     }
                 }
             }
         }
+        //int [][]matrixNew=new int[dimNew][dimNew];
 
 
         System.out.println("New matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                //if (matrix[i][j] != null)
-                System.out.printf("%4d", matrix[i][j]);
-                //   else System.out.printf("    ", matrix[i][j]);
+        boolean flag = false;
+        for (int i = 0; i < dim; i++) {
+            flag = false;
+            for (int j = 0; j < dim; j++) {
+                if (matrix[i][j] != -dim - 1) {
+                    System.out.printf("%4d", matrix[i][j]);
+                    flag = true;
+                }
             }
-            System.out.println();
+            if (flag)
+                System.out.println();
         }
-        Date dateNow = new Date();
-        Date date1 = new Date(1645086000000L);
-        System.out.println("\n©Maksim Kosach, 2022");
-        System.out.println("Date of receipt of the task: \"+date1");
-        System.out.println("Date of completion of the task: " + dateNow);
+        Main.showAuthorAndDate(date1);
     }
+
 }
